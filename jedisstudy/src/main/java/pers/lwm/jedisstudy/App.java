7143ef1a7c2495997b01c03ac150f001c80c491f
liuwma <liuwma@yonyou.com>
2017-04-11 17:35:50 +0800

    init

---
+++ jedisstudy/src/main/java/pers/lwm/jedisstudy/App.java
@@ -0,0 +1,13 @@
+package pers.lwm.jedisstudy;
+
+/**
+ * Redis设置密码与集群
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
